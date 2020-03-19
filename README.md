e5fe6b19b77c76eb3b0550bed6c4f1bf1a41a6f5
DisposaBoy <disposaboy@kuroku.io>
2020-03-19 15:20:06 +0000

    I want to believe...

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # certmagic-storage-tests
+
 Package tests implements a suite of tests for certmagic.Storage
+
+# Install
+
+    go get github.com/oyato/certmagic-storage-tests
+
+# Usage
+
+_This is package a for testing, so be sure to only import it inside your \_test.go file._
+
+    package storage
+
+    import (
+    	tests "github.com/oyato/certmagic-storage-tests"
+    	"testing"
+    )
+
+    func TestStorage(t *testing.T) {
+    	// set up your storage
+    	storage := NewInstanceOfYourStorage()
+    	// then run the tests on it
+    	tests.NewTestSuite(storage).Run(t)
+    }
+
+# Note
+
+At this time, it's an exported version of tests used for https://github.com/oyato/certmagic-storage-badger and might be incomplete or unsuitable for testing other storage implementations.
---
+++ suite.go
@@ -0,0 +1,223 @@
+// Package tests implements a suite of tests for certmagic.Storage
+//
+//
+// Example Usage:
+//
+// package storage
+//
+// import (
+//     tests "github.com/oyato/certmagic-storage-tests"
+//     "testing"
+// )
+//
+// func TestStorage(t *testing.T) {
+//     // set up your storage
+//     storage := NewInstanceOfYourStorage()
+//     // then run the tests on it
+//     tests.NewTestSuite(storage).Run(t)
+// }
+//
+package tests
+
+import (
+	"bytes"
+	"github.com/caddyserver/certmagic"
+	"math/rand"
+	"runtime"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+	"testing"
+)
+
+var (
+	// KeyPrefix is prepended to all tested keys.
+	// If changed, it must not contain a forward slash (/)
+	KeyPrefix = "__test__key__:"
+)
+
+// Suite implements tests for certmagic.Storage.
+//
+// Users should call Suite.Run() in their storage_test.go file.
+type Suite struct {
+	S   certmagic.Storage
+	Rng interface{ Int() int }
+
+	mu       sync.Mutex
+	randKeys []string
+}
+
+// Run tests the Storage
+//
+// NOTE: t.Helper() is not called.
+//       Test failure line numbers will be reported on files inside this package.
+func (ts *Suite) Run(t *testing.T) {
+	t.Cleanup(func() {
+		ts.mu.Lock()
+		defer ts.mu.Unlock()
+
+		for _, k := range ts.randKeys {
+			ts.S.Delete(k)
+		}
+	})
+	ts.testLocker(t)
+	ts.testStorageSingleKey(t)
+	ts.testStorageDir(t)
+}
+
+func (ts *Suite) testLocker(t *testing.T) {
+	key := strconv.Itoa(ts.Rng.Int())
+	if err := ts.S.Unlock(key); err == nil {
+		t.Fatalf("Storage successfully unlocks unlocked key")
+	}
+	if err := ts.S.Lock(key); err != nil {
+		t.Fatalf("Storage fails to lock key: %s", err)
+	}
+	if err := ts.S.Unlock(key); err != nil {
+		t.Fatalf("Storage fails to unlock locked key: %s", err)
+	}
+
+	test := func(key string) {
+		for i := 0; i < 10; i++ {
+			if err := ts.S.Lock(key); err != nil {
+				// certmagic lockers can timeout
+				continue
+			}
+			runtime.Gosched()
+			if err := ts.S.Unlock(key); err != nil {
+				t.Fatalf("Storage.Unlock failed: %s", err)
+			}
+		}
+	}
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		key := ts.randKey()
+		for j := 0; j < 10; j++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				test(key)
+			}()
+		}
+	}
+	wg.Wait()
+}
+
+func (ts *Suite) testStorageSingleKey(t *testing.T) {
+	key := ts.randKey()
+	val := []byte(key)
+	sto := ts.S
+	sto.Lock(key)
+	defer sto.Unlock(key)
+
+	if sto.Exists(key) {
+		t.Fatalf("Un-stored key %s exists", key)
+	}
+
+	if _, err := sto.Load(key); err == nil {
+		t.Fatalf("Load(%s) should fail: the key was not stored", key)
+	}
+
+	if err := sto.Store(key, nil); err != nil {
+		t.Fatalf("Store(%s) with `nil` value failed: %s", key, err)
+	}
+
+	if err := sto.Store(key, []byte{}); err != nil {
+		t.Fatalf("Store(%s) with empty value failed: %s", key, err)
+	}
+
+	if err := sto.Store(key, val); err != nil {
+		t.Fatalf("Store(%s) failed: %s", key, err)
+	}
+
+	if !sto.Exists(key) {
+		t.Fatalf("Stored key %s doesn't exists", key)
+	}
+
+	switch s, err := sto.Load(key); {
+	case err != nil:
+		t.Fatalf("Load(%s) failed: %s", key, err)
+	case !bytes.Equal(val, s):
+		t.Fatalf("Load(%s) failed: loaded %q != stored %q", key, s, val)
+	}
+
+	if err := sto.Delete(key); err != nil {
+		t.Fatalf("Delete(%s) failed: %s", key, err)
+	}
+
+	if sto.Exists(key) {
+		t.Fatalf("Deleted key still %s exists", key)
+	}
+
+	if err := sto.Delete(key); err == nil {
+		t.Fatalf("Delete(%s) should fail: the key was already deleted", key)
+	}
+}
+
+func (ts *Suite) testStorageDir(t *testing.T) {
+	sto := ts.S
+	dir := ts.randKey()
+	val := []byte(dir)
+	k1 := dir + "/k1"
+	k2 := dir + "/k/2"
+	ts.mu.Lock()
+	ts.randKeys = append(ts.randKeys, k1, k2)
+	ts.mu.Unlock()
+
+	if err := sto.Store(k1, val); err != nil {
+		t.Fatalf("Store(%s) failed: %s", k1, err)
+	}
+	if err := sto.Store(k2, val); err != nil {
+		t.Fatalf("Store(%s) failed: %s", k2, err)
+	}
+
+	switch inf, err := sto.Stat(dir); {
+	case err != nil:
+		t.Fatalf("Stat(%s) failed: %s", dir, err)
+	case inf.Key != dir:
+		t.Fatalf("Stat(%s) failed: Key is set to %q", dir, inf.Key)
+	case inf.IsTerminal:
+		t.Fatalf("Stat(%s) failed: IsTerminal should be false for directory keys", dir)
+	}
+
+	switch inf, err := sto.Stat(k2); {
+	case err != nil:
+		t.Fatalf("Stat(%s) failed: %s", k2, err)
+	case inf.Key != k2:
+		t.Fatalf("Stat(%s) failed: Key is set to %q", k2, inf.Key)
+	case !inf.IsTerminal:
+		t.Fatalf("Stat(%s) failed: IsTerminal should be true for non-directory keys")
+	}
+
+	switch ls, err := sto.List(dir, false); {
+	case err != nil:
+		t.Fatalf("List(%s, false) failed: %s", dir, err)
+	case len(ls) != 1 || ls[0] != "k1":
+		t.Fatalf("List(%s, false) failed: it should return [k1], not %v", dir, ls)
+	}
+
+	if ls, err := sto.List(dir, true); err != nil {
+		t.Fatalf("List(%s, true) failed: %s", dir, err)
+	} else {
+		sort.Strings(ls)
+		got := strings.Join(ls, ",")
+		exp := "k/2,k1"
+		if got != exp {
+			t.Fatalf("List(%s, true) failed: it should return [%s], not [%s]", dir, exp, got)
+		}
+	}
+}
+
+func (ts *Suite) randKey() string {
+	return KeyPrefix + strconv.Itoa(ts.Rng.Int())
+}
+
+// NewTestSuite returns a new Suite initalised with storage s
+// and a `rand.New(rand.NewSource(0))` random number generator
+func NewTestSuite(s certmagic.Storage) *Suite {
+	return &Suite{
+		S:   s,
+		Rng: rand.New(rand.NewSource(0)),
+	}
+}
